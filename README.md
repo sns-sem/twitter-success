84cc00cefee31023706ba5d4123747a3264aebd1
Sem <shadowboost98@gmail.com>
2019-07-29 06:24:46 +0200

    added state deletion

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Takes images from a Discord channel and uploads them onto Twitter. This package has album support, meaning when users post multiple images within a prefined amount of time (timer resets when new image is received), it'll bundle into a single tweet album. Maximum album size is 4.
 
+
+**New 07/29**: Deleting an image in discord while the image is in state will remove it from state, meaning it won't be posted on Twitter. (Default 5 min)
+
 ### Usage
 
 After downloading, install dependencies with `npm install` or `yarn`.
--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const bot = new Discord.Client();
 const twitter = new (require('./lib/twitter'))();
 require('dotenv').config();
 
-bot.on('message', msg => {
+bot.on('message', (msg) => {
   if (msg.channel.id !== process.env.CHANNEL_ID) return;
   if (msg.attachments.size === 0) return;
 
-  msg.attachments.forEach(attachment => {
+  msg.attachments.forEach((attachment) => {
     if (!/\.png|\.jpg/i.test(attachment.filename)) return;
-    twitter.addImage(attachment.url, msg.author);
+    twitter.addImage(attachment.url, msg.id, msg.author);
   });
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.on('messageDelete', msg => {
+  if (msg.channel.id !== process.env.CHANNEL_ID) return;
+  twitter.deleteImage(msg.id, msg.author);
+});
+
+bot.login(process.env.BOT_TOKEN).then(() => console.log(`${bot.user.tag} is running`));
